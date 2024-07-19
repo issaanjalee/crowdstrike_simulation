540873438e6ebcd5c9c986b0bd9405f241dc1690
issaanjalee <68030079+issaanjalee@users.noreply.github.com>
2024-07-19 18:37:44 +0400

    Delete crowdstrike directory

--- crowdstrike/setup.py
+++
@@ -1,9 +0,0 @@
-# setup.py
-from cx_Freeze import setup, Executable
-
-setup(
-    name="test",
-    version="1.0",
-    description="test",
-    executables=[Executable("test.py")]
-)
--- crowdstrike/test.py
+++
@@ -1,6 +0,0 @@
-# my_script.py
-def hello():
-    print("Hello, world!")
-
-if __name__ == "__main__":
-    hello()
